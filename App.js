a8cc1e59e59e5e5942499b93c225ac018e8dc1d0
naijatek21 <31557778+naijatek21@users.noreply.github.com>
2021-06-02 15:32:15 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import StandardForm from './components/ContactForm.js';
+function App() {
+  return (
+    <div className="App">
+      <StandardForm condensed="true" />
+    </div>
+  );
+}
+
+export default App;
